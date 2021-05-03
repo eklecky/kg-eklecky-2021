9de052beb39e8ba3e4a7a9f9dd2549aeb55d8f9f
Efraim Klecky <efraim@klecky.org>
2021-05-03 16:16:35 -0400

    The basic solution using modulus operator

---
+++ Phonetic-Equivalent.js
@@ -0,0 +1,17 @@
+var strArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+
+function IntToString(nums){
+    for(var i = 0; i < nums.length; i++){
+
+        currentNum = nums[i];
+
+
+        while (currentNum > 0){
+            var digit = (num % 10)
+
+            currentNum = Math.floor((num / 10))
+        }
+    }
+}
+
+
