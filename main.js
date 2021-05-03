d2ce1c40913c2b5f22675d2f51aa5864592e43c3
Efraim Klecky <efraim@klecky.org>
2021-05-03 16:47:05 -0400

    Added comments

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+// String array that integers will be mapped to
 var strArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
 
 function IntToString(nums){
     for(var i = 0; i < nums.length; i++){
 
-        currentNum = nums[i];
+        // Set variable to current element in inputted array
+        var currentNum = nums[i];
 
         var numString = "";
 
         while (currentNum > 0){
+
+            // Use modulo division to isolate individual digits
             var digit = (currentNum % 10)
 
+            // Map digits to existing String array
             numString = strArray[digit]+ numString;
 
             currentNum = Math.floor((currentNum / 10))
